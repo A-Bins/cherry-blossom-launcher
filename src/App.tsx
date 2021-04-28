9a02359e5de260fceb21cf3860cfaf1ddc57e3a2
A-Bins <yyangsuk33@naver.com>
2021-04-28 19:37:41 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Cherry">
+      <header className="Blossom">
+        <p>
+          이이이기모리
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
