06325ae1e333eeef002f1dc4390abb2232f45068
A-Bins <yyangsuk33@naver.com>
2021-07-20 17:09:47 +0900

    옵션바 추가

--- src/client.tsx
+++ src/client.tsx
@@ -4,31 +4,58 @@ import { Link } from 'react-router-dom';
 import "./client.css"
 import Info from "./user/info"
 
+interface states{
+    optionValue: string
+}
+class Client extends React.Component<any, states>{
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            optionValue: "∧"
+        }
+    }
+    
+    optionRef: (HTMLDivElement | null)[] = []
+    optionSpread() {
+        this.optionRef[0]!.style.display = "flex"
+        this.optionRef[0]!.style.animation = "down 0.5s forwards"
 
-
-class Client extends React.Component<{}>{
-
-
+        this.setState({
+            optionValue: "∨"
+        })
+    }
+    optionFolding() {
+        this.optionRef[0]!.style.animation = "up 0.5s forwards"
+        this.setState({
+            optionValue: "∧"
+        })
+    }
     render(){
         
         const hoverRef: (HTMLDivElement | null)[] = []
         function hover() {
-            console.log("시작")
             hoverRef[0]!.style.display = "inline"
         }
         function hoverDone() {
             hoverRef[0]!.style.display = "none"
         }
-        const name: string = Info.userData.name === "" ? "A_basdasdasdins" : Info.userData.name;
+        const name: string = Info.userData.name === "" ? "A_bins" : Info.userData.name;
         const id: string = Info.userData.uuid === "" ? "cbe06212-6680-4775-ad3b-7395b18777f0" : Info.userData.uuid
         return(
             <div className="Client">
                 <div className="Title">
                     <div className="profile">                    
                         <img alt="profileImg" src={`https://crafatar.com/avatars/${Info.userData.uuid === '' ? "cbe06212-6680-4775-ad3b-7395b18777f0" : Info.userData.uuid}?size=50`} className="profileImg"></img>
-                        <div className="name" onMouseLeave={ (e) => hoverDone()} onMouseEnter={ (e) => hover()}>{name}</div>
+                        <div className="namebar">
+                            <div className="name" onMouseLeave={ (e) => hoverDone()} onMouseEnter={ (e) => hover()}>{name}</div>
+                        </div>
                         <div className="id" ref={(e) => hoverRef[0] = e}>{id}</div>
-                        <div className="option"></div>
+                        <div className="option" onClick={() => this.state.optionValue === "∨" ? this.optionFolding() : this.optionSpread()}>{this.state.optionValue}</div>
+                        <div className="options" ref={(e) => this.optionRef[0] = e}>
+                            <div className="JVM">
+                                대충 JVM 설정하는거임 호호호호호호호호호호호
+                            </div>
+                        </div>
                     </div>
                     <span className="underTitle"></span>
                     <input className="start" value="시작" type="button"></input>
@@ -39,6 +66,7 @@ class Client extends React.Component<{}>{
     }
 
 }
+
 
 
 export default Client;
